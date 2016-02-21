dde1ac7a39901158690bdb877d127c291ced0409
Alexander <alex.dem.v@gmail.com>
2016-02-21 21:15:38 +0300

    checking some sh!t from msdn

---
+++ TCPclient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPclient
+{
+    public class Program
+    {
+        private const int portNum = 13;
+        private const string hostName = "host.contoso.com";
+
+        public static int Main(String[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient(hostName, portNum);
+
+                NetworkStream ns = client.GetStream();
+
+                byte[] bytes = new byte[1024];
+                int bytesRead = ns.Read(bytes, 0, bytes.Length);
+
+                Console.WriteLine(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+
+                client.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return 0;
+        }
+}
