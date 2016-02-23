468c25241511608f60c75e19ae753443d43e8a7a
Alexander <alex.dem.v@gmail.com>
2016-02-23 21:13:12 +0300

    big delay(about 40 sec)

--- TCPserver/Program.cs
+++ TCPserver/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Net.Sockets;
 using System.Net;
 using System.Threading;
-using System.Text.RegularExpressions;
-using System.IO;
 
 namespace TCPserver
 {
@@ -16,32 +13,16 @@ namespace TCPserver
             string Request = "";
             byte[] Buffer = new byte[1024];
             int Count;
-            while ((Count = Client.GetStream().Read(Buffer, 0, Buffer.Length)) > 0)
-            {
-
-                Request += Encoding.ASCII.GetString(Buffer, 0, Count);
-                // Запрос должен обрываться последовательностью \r\n\r\n
-                // Либо обрываем прием данных сами, если длина строки Request превышает 4 килобайта
-                // Нам не нужно получать данные из POST-запроса (и т. п.), а обычный запрос
-                // по идее не должен быть больше 4 килобайт
-                break;
-                if (Request.IndexOf("\r\n\r\n") >= 0 || Request.Length > 4096)
-                {
-                    break;
-                }
-            }
-
-            //Request = Request.Trim(new char[] { ',', '.' }); //удаление точен и запятых            while (Request.Contains("  ")) //пока есть двойные пробелы
-            Request = Request.Replace(".", " ");
+            Count = Client.GetStream().Read(Buffer, 0, Buffer.Length);
+            Request += Encoding.ASCII.GetString(Buffer, 0, Count);    
+            Request = Request.Replace(",", " ");
             Request = Request.Replace("  ", " ");
-
 
             string[] textArray = Request.Split(new char[] { ' ' });
 
             Console.WriteLine(Request);
             Console.WriteLine("words num: " + textArray.Length);
-            
-            
+                      
             byte[] HeadersBuffer = Encoding.ASCII.GetBytes(textArray.Length.ToString());
             Client.GetStream().Write(HeadersBuffer, 0, HeadersBuffer.Length);
            
@@ -55,19 +36,18 @@ namespace TCPserver
 
         public Server(int Port)
         {
-            IPAddress ipaddress = IPAddress.Parse("192.168.0.103");
-        //    Listener = new TcpListener(ipaddress, Port); 
             Listener = new TcpListener(IPAddress.Any, Port); 
             Listener.Start();
-
-            while (true)
+            int i = 0;
+            while (i <= 8)
             {
                 ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), Listener.AcceptTcpClient());
-
                 TcpClient Client = Listener.AcceptTcpClient();
                 Thread Thread = new Thread(new ParameterizedThreadStart(ClientThread));
                 Thread.Start(Client);
-                
+                i++;
+                Thread.Sleep(1000);
+
             }
         }
 
@@ -86,9 +66,9 @@ namespace TCPserver
 
         public static void Main(string[] args)
         {
-            int MaxThreadsCount = Environment.ProcessorCount * 4;
-            ThreadPool.SetMaxThreads(MaxThreadsCount, MaxThreadsCount);
-            ThreadPool.SetMinThreads(2, 2);
+          //  int MaxThreadsCount = Environment.ProcessorCount * 4;
+          //  ThreadPool.SetMaxThreads(MaxThreadsCount, MaxThreadsCount);
+          //  ThreadPool.SetMinThreads(2, 2);
             new Server(9050);
         }
     }
